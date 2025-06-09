62f7bc7c850cbd432d2585e5d489b82645f96e84
luanfelipe11BTI ribeiro <kreekshotpc69@gmail.com>
2025-06-09 00:38:14 +0000

    primeira parte da funcao remove item

--- main.js
+++ main.js
@@ -30,10 +30,24 @@ function showItemsList() {
                     <label for="item-${index}">${item.name}</label>
                 </div>
 
-                <button>
+                 <button onclick ="removeItem(${index})">
                     <img src="./assets/trash-icon.svg" alt="trash icon">
                 </button>
             </div>
             `
         })
+}
+
+function removeItem (index) {
+    
+
+function removeItem(itemName) {
+const itemIndex = items.findIndex((item) => item.name)
+    if (itemIndex !== -1) {
+        items.splice (itemIndex, 1)
+}
+
+    showItemsList()
+
+}
 }
